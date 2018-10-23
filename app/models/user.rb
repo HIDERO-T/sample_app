361f19224bbf02069a32ecaf82968b2e9a04954b
example <ex@mple.com>
2018-10-23 15:17:09 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
                                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_many :microposts, dependent: :destroy
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -55,6 +56,10 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # feed trial ver
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   private
   def downcase_email
     self.email.downcase!
